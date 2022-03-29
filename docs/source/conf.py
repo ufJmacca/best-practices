9028e9a4278b02fccb2af8821e9d490bc673c90f
ufJmacca <nitro.jmacca@gmail.com>
2022-03-29 21:49:39 +1100

    Added MarkDown support and mermaid for diagram making

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,8 @@ author = 'Jon McMillin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
+    'sphinxcontrib.mermaid'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
